b3e4b5b565d76df9cc5cc13c4e96f3192b81074f
Ubuntu <ubuntu@ip-172-31-34-125.us-east-2.compute.internal>
2019-08-10 14:26:06 +0000

    improve ui

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -43,7 +43,7 @@ class AttendancesController < ApplicationController
   def update
     respond_to do |format|
       if @attendance.update(attendance_params)
-        format.html { redirect_to @attendance, notice: 'Attendance was successfully updated.' }
+        format.html { redirect_to "/students/"+@attendance.student_id.to_s, notice: 'Attendance was successfully updated.' }
         format.json { render :show, status: :ok, location: @attendance }
       else
         format.html { render :edit }
