f31bd3f33278c69110f8b98fd8003fbbc47a7686
root <root@vps.server.local>
2019-08-10 06:56:19 +0000

    deployed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,9 +10,9 @@ Rails.application.config do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  # Show full error reports and disable caching.
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
