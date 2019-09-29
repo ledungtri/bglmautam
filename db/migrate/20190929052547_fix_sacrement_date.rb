c4a4c0b94d541d867c1eef61049db3d2230f47e8
Tri Le <tri.le@netcompany.com>
2019-09-29 13:08:58 +0700

    fix sacrement dates

---
+++ db/migrate/20190929052547_fix_sacrement_date.rb
@@ -0,0 +1,28 @@
+class FixSacrementDate < ActiveRecord::Migration[5.0]
+  def change
+    Student.all.each do |student| 
+      changed = false
+
+      student.date_communion
+      student.date_confirmation
+      student.date_declaration
+
+      if student.place_communion && student.place_communion == "Mẫu Tâm" && student.date_communion && student.date_communion.month != 6 && student.date_communion.month != 7
+        student.date_communion = Date.new(student.date_communion.year, student.date_communion.day, student.date_communion.month)
+        changed = true
+      end
+      if student.place_confirmation && student.place_confirmation == "Mẫu Tâm" && student.date_confirmation && student.date_confirmation.month != 6 && student.date_confirmation.month != 7 
+        student.date_confirmation = Date.new(student.date_confirmation.year, student.date_confirmation.day, student.date_confirmation.month)
+        changed = true
+      end
+      if student.place_declaration && student.place_declaration == "Mẫu Tâm" && student.date_declaration && student.date_declaration.month != 6 && student.date_declaration.month != 7
+        student.date_declaration = Date.new(student.date_declaration.year, student.date_declaration.day, student.date_declaration.month)
+        changed = true
+      end
+
+      if changed
+        student.save!
+      end
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190929041124) do
+ActiveRecord::Schema.define(version: 20190929052547) do
 
   create_table "attendances", force: :cascade do |t|
     t.string   "result"
