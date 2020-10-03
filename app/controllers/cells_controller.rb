04e6c81e2831a4315be122d8c569921cab10680f
Tri Le <tri.le@netcompany.com>
2020-10-04 00:10:00 +0700

    unfilter cells

--- app/controllers/cells_controller.rb
+++ app/controllers/cells_controller.rb
@@ -6,7 +6,7 @@ class CellsController < ApplicationController
   # GET /cells
   # GET /cells.json
   def index
-    @cells = Cell.where(year: @current_year).where(grade: ["Khai Tâm", "Rước Lễ", "Thêm Sức", "Bao Đồng"]).sort_by {|c| c.sort_param}
+    @cells = Cell.where(year: @current_year).sort_by {|c| c.sort_param}
     # TODO: add isClass field to cell
     
     respond_to do |format|
