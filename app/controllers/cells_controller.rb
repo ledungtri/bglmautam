b3375d7204ecde8b0d1bf04c55eb5320e1e79ea9
Bill Le <bill@wicket.io>
2022-10-14 06:20:32 -0400

    Create compact students pdf  view

--- app/controllers/cells_controller.rb
+++ app/controllers/cells_controller.rb
@@ -30,7 +30,8 @@ class CellsController < ApplicationController
         ] = "attachment; filename=#{@cell.name}.xlsx"
       end
       format.pdf do
-        pdf = StudentsPdf.new(@attendances, "Danh Sách Lớp #{@cell.name}\nNăm Học #{@cell.long_year}")
+        pdfClass = params[:style] == 'compact' ? CompactStudentsPdf : StudentsPdf
+        pdf = pdfClass.new(@attendances, "Danh Sách Lớp #{@cell.name}\nNăm Học #{@cell.long_year}")
         send_data pdf.render, filename: "Danh Sách Lớp #{@cell.name} Năm Học #{@cell.long_year}.pdf", type: 'application/pdf', disposition: 'inline'
       end
     end
---
+++ app/pdfs/compact_students_pdf.rb
@@ -0,0 +1,78 @@
+class CompactStudentsPdf < Prawn::Document
+  def initialize(attendances, title_text)
+    super(page_size: 'A4', page_layout: :portrait, margin: 20)
+    @attendances = attendances
+    @title_text = title_text
+    self.font_size = 8
+
+    font_families.update('HongHa' => {
+      normal: Rails.root.join('app/assets/fonts/UVNHongHa_R.TTF'),
+      bold: Rails.root.join('app/assets/fonts/14_13787_UVNHongHa_B.TTF')
+    })
+    font 'HongHa'
+
+    title
+    move_down 20
+    line_items
+    footer
+  end
+
+  def title
+    text @title_text, size: 15, align: :center, style: :bold
+    text "Số Lượng: #{@attendances.count}", size: 10, align: :center
+  end
+
+  def line_items
+    bounding_box([0, cursor], width: bounds.width, height: bounds.height - 80) do
+      @attendances.group_by(&:result).sort.each do |key, group|
+        @group = group
+        table line_item_rows do
+          self.header = true
+          self.position = :center
+          self.row_colors = %w[e5e3e3 FFFFFF]
+
+          row(0).style(align: :center, font_style: :bold, height: 35, valign: :center)
+
+          column(0).style(align: :center)
+          column(1).style(width: 220)
+          column(2).style(width: 100, align: :center)
+          column(3).style(width: 100, align: :center)
+          column(4).style(width: 100, align: :center)
+        end
+        bounding_box([bounds.width - 60, cursor], width: 60) do
+          text "#{key}: #{group.count}", align: :center
+          move_down 10
+        end
+      end
+    end
+  end
+
+  def line_item_rows
+    [['STT', 'Tên Thánh - Họ và Tên', 'Ngày Sinh', 'Lớp', 'Kết Quả']] +
+      @group.each_with_index.map do |attendance, index|
+        student = attendance.student
+        [
+          index + 1,
+          student.name,
+          student.date_birth.strftime('%d/%m/%Y'),
+          attendance.cell.name,
+          attendance.result
+        ]
+      end
+  end
+
+  def footer
+    page_count.times do |i|
+      go_to_page(i + 1)
+      bounding_box [bounds.left, bounds.bottom + 10], width: bounds.width do
+        text 'Ban Giáo Lý Giáo Xứ  Mẫu Tâm', size: 6, align: :right
+      end
+      bounding_box [bounds.left, bounds.bottom + 10], width: bounds.width do
+        text "In ngày: #{Time.zone.now.strftime('%d/%m/%Y')}", size: 6, align: :left
+      end
+      bounding_box [bounds.left, bounds.bottom + 10], width: bounds.width do
+        text "Trang #{i + 1}/#{page_count}", size: 6, align: :center
+      end
+    end
+  end
+end
