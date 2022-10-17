9d3bd6d674da815b58bd6adb75b808ce94664d8e
Bill Le <bill@wicket.io>
2022-10-16 23:44:45 -0400

    Teachers contact Pdf

--- app/controllers/cells_controller.rb
+++ app/controllers/cells_controller.rb
@@ -30,6 +30,11 @@ class CellsController < ApplicationController
         ] = "attachment; filename=#{@cell.name}.xlsx"
       end
       format.pdf do
+        if params[:style] == 'teachers_contact'
+          pdf = TeachersContactPdf.new(@cell)
+          return send_data pdf.render, filename: "Số điện toại GLV.pdf", type: 'application/pdf', disposition: 'inline'
+        end
+
         pdfClass = params[:style] == 'compact' ? CompactStudentsPdf : StudentsPdf
         pdf = pdfClass.new(@attendances, "Danh Sách Lớp #{@cell.name}\nNăm Học #{@cell.long_year}")
         send_data pdf.render, filename: "Danh Sách Lớp #{@cell.name} Năm Học #{@cell.long_year}.pdf", type: 'application/pdf', disposition: 'inline'
---
+++ app/pdfs/teachers_contact_pdf.rb
@@ -0,0 +1,43 @@
+class TeachersContactPdf < Prawn::Document
+  def initialize(cell)
+    super(page_size: 'A4')
+    self.font_size = 14
+    font_families.update('HongHa' => {
+      normal: Rails.root.join('app/assets/fonts/UVNHongHa_R.TTF'),
+      bold: Rails.root.join('app/assets/fonts/14_13787_UVNHongHa_B.TTF')
+    })
+    font 'HongHa'
+
+    title = "<font size='18'><b>#{cell.name}</b></font>"
+
+    body = cell.instructions.reduce('') do |content, instruction|
+      content += "<b>#{instruction.position}</b>: #{instruction.teacher.full_name}\n<b>SĐT</b>: #{instruction.teacher.phone}\n"
+      content
+    end
+
+    (cell.instructions.length >= 3 ? 4 : 5).times.each do
+      table [[title, '', title]] + [[body, '', body]] do
+        self.position = :center
+        self.width = 550
+        self.cells.border_width = 0
+        self.cell_style = { inline_format: true }
+        self.before_rendering_page do |page|
+          page.row(0).border_top_width = 1
+          page.row(-1).border_bottom_width = 1
+          page.column(0).border_left_width = 1
+          page.column(0).border_right_width = 1
+          page.column(1).border_top_width = 0
+          page.column(1).border_bottom_width = 0
+          page.column(-1).border_left_width = 1
+          page.column(-1).border_right_width = 1
+        end
+
+        row(0).style(align: :center)
+        row(0).style(align: :center)
+        row(1).style(align: :left)
+      end
+
+      move_down 20
+    end
+  end
+end
