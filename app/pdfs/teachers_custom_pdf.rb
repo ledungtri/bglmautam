13c40911ac2b1a6267bd1909743244ce93e6bf77
Bill Le <bill@wicket.io>
2023-09-11 04:27:52 -0400

    Update teacher custom export

--- app/pdfs/teachers_custom_pdf.rb
+++ app/pdfs/teachers_custom_pdf.rb
@@ -34,7 +34,7 @@ class TeachersCustomPdf < Prawn::Document
 
         column(0).style(align: :center)
         column(0).style(width: 30)
-        column(1).style(width: 220)
+        column(3).style(width: 220)
       end
     end
   end
