c1abf99484c77b57f77342ecdcf9bfd7b4865fa6
Bill Le <bill@wicket.io>
2024-05-25 01:49:40 -0400

    Update session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,19 +4,20 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_username(params[:username])
-    if user&.authenticate(params[:password])
-      session[:user_id] = user.id
-      flash[:success] = 'Logged in'
-      if user.teacher
-        classroom_id = Guidance.for_year(@current_year).where(teacher: user.teacher).pluck(:classroom_id).first
-        redirect_to classroom_url(classroom_id) if classroom_id
-      else
-        redirect_to root_url
-      end
-    else
+
+    unless user&.authenticate(params[:password])
       flash.now[:warning] = 'Username or password is invalid'
-      render 'new'
+      return render 'new'
     end
+
+    session[:user_id] = user&.id
+    flash[:success] = 'Logged in'
+    if user&.teacher
+      classroom_id = Guidance.for_year(@current_year).where(teacher: user.teacher).pluck(:classroom_id).first
+      return redirect_to classroom_url(classroom_id) if classroom_id
+    end
+
+    redirect_to root_url
   end
 
   def destroy
