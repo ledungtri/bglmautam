12e5a4cd248f71fef6d1726fa20f72e7d1b5db71
Bill Le <bill@wicket.io>
2024-06-12 13:23:29 -0400

    Inline select partial

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,4 +2,8 @@ class ApplicationRecord < ActiveRecord::Base
   acts_as_paranoid
 
   self.abstract_class = true
+
+  def attribute_names
+    super - %w[id created_at updated_at deleted_at]
+  end
 end
