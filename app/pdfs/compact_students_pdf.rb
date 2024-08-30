44bb7a9738e92e107f74aa4553805d9b278c0d2e
Bill Le <bill@wicket.io>
2024-08-30 14:41:48 -0400

    update compact students pdf

--- app/pdfs/compact_students_pdf.rb
+++ app/pdfs/compact_students_pdf.rb
@@ -2,36 +2,34 @@ class CompactStudentsPdf < AbstractPdf
 
   def initialize(enrollments, title_text)
     @enrollments = enrollments
+    @last_year = @enrollments.first.classroom.year - 1
     sub_title_text = "Số Lượng: #{@enrollments.count}"
     super(:portrait, title_text, sub_title_text)
   end
 
   def body
     bounding_box([0, cursor], width: bounds.width, height: bounds.height - 78) do
-      @enrollments.group_by(&:result).sort.each do |key, group|
-        @group = group
-        table line_items do
-          self.header = true
-          self.position = :center
-          self.row_colors = %w[e5e3e3 FFFFFF]
+      table line_items do
+        self.header = true
+        self.position = :center
+        self.row_colors = %w[e5e3e3 FFFFFF]
 
-          row(0).style(align: :center, font_style: :bold, valign: :center)
+        row(0).style(align: :center, font_style: :bold, valign: :center)
 
-          column(0).style(align: :center)
-          column(1).style(width: 220)
-          column(2).style(width: 100, align: :center)
-          column(3).style(width: 100, align: :center)
-          column(4).style(width: 100, align: :center)
-        end
+        column(0).style(align: :center)
+        column(1).style(width: 220)
+        column(2).style(width: 100, align: :center)
+        column(3).style(width: 100, align: :center)
+        column(4).style(width: 100, align: :center)
       end
     end
   end
 
   def line_items
-    [['STT', 'Tên Thánh - Họ và Tên', 'Ngày Sinh', 'Lớp', 'Kết Quả']] +
-      @group.each_with_index.map do |enrollment, index|
+    [['STT', 'Tên Thánh - Họ và Tên', 'Ngày Sinh', 'Lớp Năm Ngoái', 'Kết Quả']] +
+      @enrollments.each_with_index.map do |enrollment, index|
         student = enrollment.student
-        last_enrollment = student.enrollments.for_year(enrollment.classroom.year - 1).take
+        last_enrollment = student.enrollments.for_year(@last_year).take
         [
           index + 1,
           student.name,
