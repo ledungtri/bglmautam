64fa2fb7193b6e85bf21ce07e0a58e97f5505a3f
Bill Le <ledungtri.2202@gmail.com>
2024-09-26 21:13:34 +0000

    annotate

--- app/controllers/grades_controller.rb
+++ app/controllers/grades_controller.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: grades
+#
+#  id            :integer          not null, primary key
+#  deleted_at    :datetime
+#  name          :string
+#  value         :float
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  enrollment_id :integer
+#
+# Indexes
+#
+#  index_grades_on_enrollment_id  (enrollment_id)
+#
 class GradesController < SecondaryResourcesController
   def update
     skip_redirect
--- app/models/grade.rb
+++ app/models/grade.rb
@@ -4,6 +4,7 @@
 #
 #  id            :integer          not null, primary key
 #  deleted_at    :datetime
+#  name          :string
 #  value         :float
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
