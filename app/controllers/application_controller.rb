5028abb5d3ed5683e569823f09773b01ce2314bd
Bill Le <bill@wicket.io>
2025-09-14 16:22:40 +0700

    Update start date & end date

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,8 +24,8 @@ private
   def set_current_year
     @current_year = params[:year]&.to_i || 2025
     @current_year_long = "#{@current_year} - #{@current_year + 1}"
-    @current_year_start_date = '2024.09.15'
-    @current_year_end_date = '2025.06.15'
+    @current_year_start_date = '2025.09.07'
+    @current_year_end_date = '2026.05.31'
   end
 
   def set_current_user
