8b92abf2727bf69474186a01cf1180f3faff565e
Bill Le <bill@wicket.io>
2025-10-11 14:26:52 +0700

    Update name in teacher contact pdf

--- app/pdfs/teachers_contact_pdf.rb
+++ app/pdfs/teachers_contact_pdf.rb
@@ -11,7 +11,7 @@ class TeachersContactPdf < Prawn::Document
     title = "<font size='18'><b>#{classroom.name}</b></font>"
 
     body = classroom.guidances.sort_by(&:sort_param).reduce('') do |content, guidance|
-      content += "<b>#{guidance.position}</b>: #{guidance.teacher.full_name}\n<b>SĐT</b>: #{guidance.teacher.phone}\n"
+      content += "<b>#{guidance.position}</b>: #{guidance.teacher.name}\n<b>SĐT</b>: #{guidance.teacher.phone}\n"
       content
     end
 
