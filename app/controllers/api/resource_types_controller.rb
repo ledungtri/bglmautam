1eb5f55a81baac818ec17c27ca8e2cab02c9534e
Bill Le <bill@wicket.io>
2026-01-27 01:22:28 +0700

    Remove unused controllers

--- app/controllers/api/resource_types_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::ResourceTypesController < ApplicationController
-  skip_before_action :auth # TODO: authorize
-
-  def index
-    @resource_types = ResourceType.for_key(params[:key])
-    render json: @resource_types
-  end
-end
