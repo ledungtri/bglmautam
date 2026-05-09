3cd3b8bacc67e7a0bb593fb7d042e7d54ea597a2
Bill Le <bill@wicket.io>
2026-05-10 00:30:33 +0700

    Update excel export

--- app/services/students_excel_export.rb
+++ app/services/students_excel_export.rb
@@ -12,7 +12,18 @@ class StudentsExcelExport
       header_style = sheet.styles.add_style(b: true, alignment: { horizontal: :center })
 
       sheet.add_row(
-        ['STT', 'Họ và Tên', 'Ngày Sinh', 'Địa Chỉ', 'ĐT Cha', 'ĐT Mẹ', 'Kết Quả'],
+        [
+          'STT',
+          'Tên Thánh', 'Họ và Tên', 'Ngày Sinh', 'Nơi Sinh', 'Giới Tính',
+          'Rửa Tội', 'Nơi Rửa Tội',
+          'Rước Lễ', 'Nơi Rước Lễ',
+          'Thêm Sức', 'Nơi Thêm Sức',
+          'Tuyên Hứa', 'Nơi Tuyên Hứa',
+          'Tên Thánh Cha', 'Họ và Tên Cha', 'ĐT Cha',
+          'Tên Thánh Mẹ', 'Họ và Tên Mẹ', 'ĐT Mẹ',
+          'Số Nhà', 'Đường', 'Phường/Xã', 'Quận/Huyện', 'Xóm Giáo',
+          'Kết Quả'
+        ],
         style: header_style
       )
 
@@ -20,16 +31,31 @@ class StudentsExcelExport
 
       @enrollments.each_with_index do |enrollment, index|
         student = enrollment.student
-        sheet.add_row([
-          index + 1,
-          student.name,
-          student.date_birth&.strftime('%d/%m/%Y'),
-          student.address,
-          student.father_phone,
-          student.mother_phone,
-          enrollment.result
-        ], types: [:integer, :string, :string, :string, :string, :string, :string],
-           style: [nil, nil, nil, nil, text_style, text_style, nil])
+        sheet.add_row(
+          [
+            index + 1,
+            student.christian_name, student.full_name,
+            student.date_birth&.strftime('%d/%m/%Y'), student.place_birth, student.gender,
+            student.date_baptism&.strftime('%d/%m/%Y'), student.place_baptism,
+            student.date_communion&.strftime('%d/%m/%Y'), student.place_communion,
+            student.date_confirmation&.strftime('%d/%m/%Y'), student.place_confirmation,
+            student.date_declaration&.strftime('%d/%m/%Y'), student.place_declaration,
+            student.father_christian_name, student.father_full_name, student.father_phone,
+            student.mother_christian_name, student.mother_full_name, student.mother_phone,
+            student.street_number, student.street_name, student.ward, student.district, student.area,
+            enrollment.result
+          ],
+          types: Array.new(27, :string).unshift(:integer),
+          style: [
+            nil,
+            nil, nil, nil, nil, nil, text_style,
+            nil, nil, nil, nil, nil, nil, nil, nil,
+            nil, nil, text_style,
+            nil, nil, text_style,
+            nil, nil, nil, nil, nil,
+            nil
+          ]
+        )
       end
     end
 
